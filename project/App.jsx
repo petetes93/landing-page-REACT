9b1711b4c6619c35b07c9d66759487670f3fb9fa
petetes93 <alejandrolobatohaurie@gmail.com>
2023-11-16 09:28:38 +0100

    first commit

---
+++ project/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import info from './data.json'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
